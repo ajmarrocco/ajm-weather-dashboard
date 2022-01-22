a007735fdd60f63c664c30828b036c766f39a6a9
ajmarrocco <ajmarrocco@gmail.com>
2022-01-22 14:48:36 -0500

    console log data

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,6 @@ var getCurrent = function(city){
     // console.log("https://api.openweathermap.org/data/2.5/onecall?lat=41.85&lon=-87.65&exclude=minutely,hourly,alerts&appid=2c279aedc4b3d33df9584a1e023c4e2e")
 
     var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2c279aedc4b3d33df9584a1e023c4e2e`;
-    console.log(apiUrl);
 
     // make a get request to url
     fetch(apiUrl).then(function(response) {
@@ -27,16 +26,31 @@ var getCurrent = function(city){
         if (response.ok) {
             response.json().then(function(data) {
             displayCurrent(data);
+            getFuture(data.coord.lon,data.coord.lat)
         })
         } else {
         // if not successful, redirect to homepage
+            window.alert("Please enter a city name");
             document.location.replace("./index.html");
         }
     })
 }
 
 var displayCurrent = function(weather){
-    console.log(weather);
+    console.log("city " + weather.name);
+    console.log("date " + weather.dt);
+    console.log("temp " + weather.main.temp);
+    console.log("speed " + weather.wind.speed);
+    console.log("humidity " + weather.main.humidity);
 }
+
+var getFuture = function(longitiude, latitude){
+    console.log("longitude " + longitiude);
+    console.log("latitude " + latitude);
+}
+
+// var displayUVI = function(weather){
+
+// }
 
 searchButtonEl.addEventListener("click", getCityName);
